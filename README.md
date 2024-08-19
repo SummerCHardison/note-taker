93f9915c57589e01f1f6977192d8fa9445ef0122
Summer H <sh.hardison.sh@icloud.com>
2024-08-19 15:58:34 -0400

    Debugged some code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # note-taker
+
+## Description
+
+## Depolyment
+
+## Credits
+
+Summer Hardison
+
--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
